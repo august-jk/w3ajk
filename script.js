8ca46eaec8c53321f65c160b82284718f5d3b5db
Aletha Wheeless <augustkama98@gmail.com>
2022-04-14 13:28:12 -1000

    background image and basic text

--- script.js
+++
@@ -1,6 +0,0 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-const p = document.createElement("p");
-p.textContent = "I love JavaScript!";
-document.querySelector("body").appendChild(p);
